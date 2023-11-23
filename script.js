fdbe6423791cd80c7028b16450d820c3d1f62e89
sunnatk <144546276+sunnatk@users.noreply.github.com>
2023-11-23 19:37:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+`use strict`;
+let age = 18;
+let name1 = `Sunnat`;
+let firstname = `Zafarovich`;
+
+console.log(` ${name1} ${firstname}! `);
+
+// нужно сделать true студент
+let isStudent = true;
+console.log(isStudent);
+
+let temperature = -5;
+console.log(temperature);
+
+let city = `ST.Petersburg`;
+console.log(city);
+
+let hour = `17:11`;
+console.log(hour);
+
+// точки на плоскости
+
+let fullName = `Sunnat Zafarovich`;
+console.log(fullName);
+
+let num1 = 10;
+let num2 = 10;
+console.log(num1 + num2);
+
+let greeting = `Привет`;
+let exclamation = `!`;
+
+console.log(greeting + exclamation);
+
+
+let greetingMe = ``;
+console.log(`${greeting} ${name1}`);
